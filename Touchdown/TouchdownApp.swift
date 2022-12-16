d081471f3f82de849d6dc0d410d983a0e7c0b6f6
Baris Ciftci <barishciftci@gmail.com>
2022-12-16 19:43:58 +0100

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Baris Ciftci on 16/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
