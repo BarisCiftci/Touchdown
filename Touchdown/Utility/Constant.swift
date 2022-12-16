2ece714b8cf68eec280e8745aaad23e0b220598d
Baris Ciftci <barishciftci@gmail.com>
2022-12-16 21:17:57 +0100

    Project setup, adding assets, creating folders/files

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,23 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Baris Ciftci on 16/12/2022.
+//
+
+import SwiftUI
+//DATA
+
+//COLOR
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGrey: Color = Color(UIColor.systemGray4)
+
+//LAYOUT
+//UX
+//API
+//IMAGE
+//FONT
+//STRING
+//MISC
+
